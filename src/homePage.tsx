2d0044c831ae653d2902103c935820ca587d5f5b
Jouni Alanen <jouni@diory.me>
2025-03-05 12:43:21 +0200

    Full screen image on black background

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,5 +1,12 @@
 const HomePage = () => {
-  return "Hello world!"
+  const url =
+    "https://media.istockphoto.com/id/636379014/fi/valokuva/k%C3%A4det-muodostavat-syd%C3%A4men-muodon-auringonlaskun-siluetti.jpg?s=1024x1024&w=is&k=20&c=hCdUB-xdu_FwJNRUq23AfFSOKCqDQ8_eeLMaV9yup5s=";
+
+  return (
+    <div className="container">
+      <img src={url} />
+    </div>
+  );
 };
 
 export default HomePage;
