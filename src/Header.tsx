d8625eb2f43411ff0e37940437141b986feaa6fc
Jouni Alanen <jouni@diory.me>
2025-03-16 14:08:19 +0200

    Add diory text visible to DiorySlide

--- src/Header.tsx
+++ src/Header.tsx
@@ -11,7 +11,7 @@ const Header = ({ text, onClick }: Props) => {
     <div className={styles.headerContainer}>
       <div
         className={styles.headerSquare}
-        {/* For some reason headerSquare class is not applied properly so needed to add these inline */}
+        // For some reason headerSquare class is not applied properly so needed to add these inline
         style={{ width: "80px", height: "100%" }}
       >
         <img src="https://www.svgrepo.com/download/305142/arrow-ios-back.svg" />
@@ -21,7 +21,7 @@ const Header = ({ text, onClick }: Props) => {
       </div>
       <div
         className={styles.headerSquare}
-        {/* For some reason headerSquare class is not applied properly so needed to add these inline */}
+        // For some reason headerSquare class is not applied properly so needed to add these inline
         style={{ width: "80px", height: "100%" }}
       >
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/640px-Hamburger_icon.svg.png" />
