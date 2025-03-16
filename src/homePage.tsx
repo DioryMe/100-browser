25b3e4cd4e271dc1eecb2d0167a30f156dda6470
Jouni Alanen <jouni@diory.me>
2025-03-16 14:06:17 +0200

    Clarifying comments

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,6 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import { storyDiories } from "./App";
 import styles from "./DiorySwipes.module.css";
+
+// Home page is a custom page and has fixed links to diories
+// --- under construction ---
+
+// TODO: Iterate through storyDiories: needs diosphere context
+// const mary = "e07c2f1d-5f5a-488a-a505-34f7b9f55105"
+// const scoutsEvent = "f1470597-1a3c-4a76-8546-2c29591a6846";
+// const storyDiories = [mary, scoutsEvent];
+
+import diograph from "../mary-json.json";
+const mary = diograph["e07c2f1d-5f5a-488a-a505-34f7b9f55105"];
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,11 +19,8 @@ const HomePage = () => {
     <div className={styles.container}>
       <div>My Diory</div>
       <div>
-        {storyDiories[0].text}
-        <img
-          onClick={() => navigate(`/diory/${storyDiories[0].id}`)}
-          src={storyDiories[0].image}
-        />
+        {mary.text}
+        <img onClick={() => navigate(`/diory/${mary.id}`)} src={mary.image} />
       </div>
     </div>
   );
