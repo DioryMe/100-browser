299995b8385a5087cbd3dbc830ee794545668089
Jouni Alanen <jouni@diory.me>
2025-06-13 22:13:00 +0300

    Video works

--- src/ContentSlide.tsx
+++ src/ContentSlide.tsx
@@ -8,36 +8,30 @@ export const ContentSlide = ({ diory }: { diory: IDioryObject }) => {
   const navigate = useNavigate();
   const { storyId } = useSelector((state: RootState) => state.diory);
   // Select the blob URL from Redux using the diory id
-  const contentUrl2 = useSelector(
+  const dioryContent = useSelector(
     (state: RootState) => state.diory.contentUrls[diory.id]
   );
 
-  let content, content2;
-  if (contentUrl2) {
-    console.log("con", contentUrl2);
-    const { url: contentUrl, mimeType } = contentUrl2;
-
-    content = (
-      <img
-        // Use the blob URL if available; fallback to the diory.image otherwise.
-        src={contentUrl ? contentUrl : diory.image}
-      />
-    );
-
-    content2 = (
-      <video controls={true} loop={true} autoPlay={false}>
-        <source src={contentUrl} type="video/mp4" />
-        <track kind="captions" />
-      </video>
-    );
+  if (!dioryContent) {
+    return null;
   }
+
+  const { url: contentUrl, mimeType } = dioryContent;
 
   return (
     <div className={styles.fullImage} style={{ cursor: "grab" }}>
       <div className="swiper-zoom-container">
-        {" "}
-        {/* Added container for Swiper zoom */}
-        {content}
+        {mimeType.startsWith("video") ? (
+          <video controls loop style={{ maxHeight: "90vh", width: "100%" }}>
+            <source src={contentUrl} type={mimeType} />
+            <track kind="captions" />
+          </video>
+        ) : (
+          <img
+            // Use the blob URL if available; fallback to the diory.image otherwise.
+            src={contentUrl ? contentUrl : diory.image}
+          />
+        )}
       </div>
     </div>
   );
